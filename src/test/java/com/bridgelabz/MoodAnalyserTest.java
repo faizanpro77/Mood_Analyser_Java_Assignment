bd5c41bf977225e047241032dc5ff73c9c3d9366
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-30 18:46:30 +0530

    MODIFIED: add functionality to analyse mood and validate

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
@@ -8,6 +9,8 @@ public class MoodAnalyserTest {
     public void givenMood_whenMessageAnalyse_returnSad() {
       MoodAnalyser moodAnalyser = new MoodAnalyser();
       String mood = moodAnalyser.analyseMood("Message is sad");
+      System.out.println(mood);
+      Assert.assertEquals("Sad", mood);
 
     }
 }
